571fd84c75ea19698a3d4a24bda8948f60135eea
ghiggi <gionata.ghiggi@gmail.com>
2023-06-16 15:21:58 +0200

    Add draft dict for plotting api

---
+++ xverif/metrics/probabilistic/names.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 15 15:09:00 2023.
+
+@author: ghiggi
+"""
+# ### Probabilistic binary
+# labels_dict = {
+#     "AUC_ROC" : "Area under the ROC Curve",
+#     "AUC_PR" : "Area under the Precision-Recall Curve",
+#     "AUC_GAIN" : "Area under the Gain Curve",
+#     "AP" : "Area under the Precision-Recall Curve",
+#     "QS" : "Ensemble Adjusted Quadratic Score",
+#     "IGN" : "Ignorance Score",
+#     "BS" : "Brier Score",
+#     "Reliability" : "Reliability",
+#     "Resolution" : "Resolution",
+#     "Uncertainty" : "Uncertainty",
+#  }
+
+# ### Probabilistic multiclass
+# labels_dict = {"QS" : "Ensemble Adjusted Quadratic Score",
+#                "IGN" : "Ignorance Score",
+#                "RPS": "Rank Probability Score",
+# }
